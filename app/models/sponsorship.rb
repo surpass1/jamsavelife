066f239c0afac9b7c25adc6e346b9e2ce196750c
surpass1 <paulkawalya@yahoo.com>
2014-12-15 14:01:47 +0300

    added a dropdown in rails admin for status

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -1,4 +1,8 @@
 class Sponsorship < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def status_enum
+   	[['Sponsored'],['Not supposed'],['Banned']]
+  end
 end
