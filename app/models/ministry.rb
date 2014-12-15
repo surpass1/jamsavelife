c3b6484dc3c4b623de2785cc53b5bc35fdf4b84f
surpass1 <paulkawalya@yahoo.com>
2014-12-15 18:47:35 +0300

    fixed corousel height to 200px

--- app/models/ministry.rb
+++ app/models/ministry.rb
@@ -1,4 +1,10 @@
 class Ministry < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
+   def category_enum
+   	[['education'],['evangelism'],['children'], ['vocational']]
+   end
+   validates_presence_of :category, :title, :description
 end
