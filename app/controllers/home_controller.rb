3f28f29c2c507b5c307cd1e1eba057911309fe18
surpass1 <paulkawalya@yahoo.com>
2014-12-22 12:54:26 +0300

    added  image to donate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
   	@ministry = Home.where(category: 'ministry').last(3)
   	@event = Home.where(category: 'Workinprogress').last(3)
+  	@advert = Home.where(category: 'advert').last(1)
   end
 end
