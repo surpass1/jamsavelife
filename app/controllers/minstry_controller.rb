bd30343e85f31e359fb49d8554632fe6690c581e
surpass1 <paulkawalya@yahoo.com>
2015-01-05 01:23:28 +0300

    Fixed ministry pages

--- app/controllers/minstry_controller.rb
+++ app/controllers/minstry_controller.rb
@@ -1,21 +1,25 @@
 class MinstryController < ApplicationController
   def children
     @advert = Home.where(category: 'advert').last(1)
-  	@children = Ministry.all
+  	@children = Ministry.where(category: 'children')
+    @category = Ministry.all
   end
 
   def education
     @advert = Home.where(category: 'advert').last(1)
-  	@children = Ministry.all
+  	@children = Ministry.where(category: 'education')
+    @category = Ministry.all
   end
 
   def discipleship
     @advert = Home.where(category: 'advert').last(1)
-  	@children = Ministry.all
+  	@children = Ministry.where(category: 'evangelism')
+    @category = Ministry.all
   end
 
   def vacational
     @advert = Home.where(category: 'advert').last(1)
-  	@children = Ministry.all
+  	@children = Ministry.where(category: 'vocational')
+    @category = Ministry.all
   end
 end
