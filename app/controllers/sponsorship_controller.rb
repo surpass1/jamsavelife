4faeecf4a3cbef3bc8292ac7b56e051dc42de62a
surpass1 <paulkawalya@yahoo.com>
2015-01-05 17:38:57 +0300

    Added pagination kaminari bootstrap

--- app/controllers/sponsorship_controller.rb
+++ app/controllers/sponsorship_controller.rb
@@ -1,6 +1,6 @@
 class SponsorshipController < ApplicationController
   def index
   	@advert = Home.where(category: 'advert').last(1)
-  	@child = Sponsorship.all
+  	@child = Sponsorship.all.order("created_at").page(params[:page]).per(5)
   end
 end
