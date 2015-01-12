644642235b52d6f25d5c458a68f0631ee6aaa42a
surpass1 <paulkawalya@yahoo.com>
2015-01-12 16:33:15 +0300

    added achievements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
   	@ministry = Home.where(category: 'ministry').last(3)
+  	@achievements = Home.where(category: 'achievements').last(6)
   	@event = Home.where(category: 'Workinprogress').last(3)
   	@advert = Home.where(category: 'advert').last(1)
   end
