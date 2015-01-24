1d07940e7ac03b1617020004878f058919f71cc8
surpass1 <paulkawalya@yahoo.com>
2015-01-24 13:25:26 +0300

    added background and uganda dropdowns on home tab

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
   
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,14 +24,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
